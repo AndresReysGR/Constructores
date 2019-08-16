248cc3b82d9d16dc31bd234f9d29eaadd40c5477
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-16 08:47:58 -0700

    Agregar archivos de proyecto.

---
+++ Constructores/Contado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    class Contado
+    {
+        public string Nombre { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        
+        public Contado()
+        {
+            Nombre = "Sin nombre";
+            Celular = "Sin Celular";
+            Telefono = "Sin telefono";
+            Correo = "Sin correo";
+            Direccion = "Sin direccion";
+        }
+
+        public Contado(string nombre, string correo, string celular, string telefono, string direccion)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Celular = "Sin Celular";
+            Telefono = "Sin telefono";
+            Direccion = "Sin direccion";
+        }
+
+
+    }
+}
---
+++ Constructores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contado contado1 = new Contado();
+            contado1.Nombre = "Jose";
+            contado1.Celular = "(644) 00 00 004";
+            contado1.Correo = "jose@gmail.com";
+
+            Console.WriteLine(
+                "Nombre: " + contado1.Nombre);
+            Console.WriteLine(
+                "Correo: " + contado1.Correo);
+            Console.WriteLine(
+                "Direccion: " + contado1.Direccion);
+            Console.WriteLine(
+                "Celular: " + contado1.Celular);
+            Console.WriteLine( 
+               "Telefono: " + contado1.Telefono);
+
+            Contado contado2 =
+                new Contado("Juan", "Juan@gmail.com", "64401544", "1441541", "casacasasas");
+            Console.WriteLine("Contado 2");
+            Console.WriteLine(
+                "Nombre: " + contado2.Nombre);
+            Console.WriteLine(
+                "Correo: " + contado2.Correo);
+            Console.WriteLine(
+                "Direccion: " + contado2.Direccion);
+
+
+
+            Console.Read();
+        }
+    }
+}
